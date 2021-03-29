ba5534ff2579f51fb504be35e2e28702e1e94aa7
Matteo <matteomigliori280489@gmail.com>
2021-03-29 17:40:02 +0200

    finito es base

---
+++ vue/script.js
@@ -0,0 +1,19 @@
+var app = new Vue({
+    el:"#root",
+    data: {
+        mailList:[],
+    },
+
+    mounted() {
+        for( let i = 0; i < 10 ; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((pluto) =>{
+                    console.log(pluto);
+                    this.mailList.push(pluto.data.response);
+                    
+                })
+            }
+    }
+
+
+})
